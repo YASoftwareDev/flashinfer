YASoftwareDev/flashinfer

commit d92ede57073c144002c88a9652ca0e0db8143242

jit: adding more examples (#534)

python/flashinfer/jit/attention.py
@@ -17,6 +17,7 @@ limitations under the License.
 import torch
 import jinja2
 import os
+import pathlib
 from .env import FLASHINFER_GEN_SRC_DIR
 from .utils import (
     write_if_different,
@@ -25,10 +26,11 @@ from .utils import (
     pos_encoding_mode_literal,
     mask_mode_literal,
 )
-from .single_decode_templ import single_decode_templ
+from .single_decode_templ import single_decode_templ, customizable_single_decode_templ
 from .batch_decode_templ import batch_decode_templ
-from .single_prefill_templ import single_prefill_templ
+from .single_prefill_templ import single_prefill_templ, customizable_single_prefill_templ
 from .batch_prefill_templ import batch_prefill_templ
+from typing import List, Tuple
 
 
 def get_single_decode_cu_str(
@@ -72,15 +74,18 @@ def get_single_decode_uri(
     )
 
 
-def gen_single_decode_cu(*args) -> None:
+def gen_single_decode_cu(*args) -> Tuple[str, pathlib.Path]:
     gen_directory = FLASHINFER_GEN_SRC_DIR
     if not os.path.exists(gen_directory):
         os.makedirs(gen_directory)
-    file_name = f"{get_single_decode_uri(*args)}.cu"
+    uri = get_single_decode_uri(*args)
+    file_name = f"{uri}.cu"
+    path = gen_directory / file_name
     write_if_different(
-        gen_directory / file_name,
+        path,
         get_single_decode_cu_str(*args),
     )
+    return file_name, path
 
 
 def get_batch_decode_cu_str(
@@ -128,15 +133,18 @@ def get_batch_decode_uri(
     )
 
 
-def gen_batch_decode_cu(*args) -> None:
+def gen_batch_decode_cu(*args) -> Tuple[str, pathlib.Path]:
     gen_directory = FLASHINFER_GEN_SRC_DIR
     if not os.path.exists(gen_directory):
         os.makedirs(gen_directory)
-    file_name = f"{get_batch_decode_uri(*args)}.cu"
+    uri = get_batch_decode_uri(*args)
+    file_name = f"{uri}.cu"
+    path = gen_directory / file_name
     write_if_different(
-        gen_directory / file_name,
+        path,
         get_batch_decode_cu_str(*args),
     )
+    return uri, path
 
 
 def get_single_prefill_cu_str(
@@ -188,15 +196,18 @@ def get_single_prefill_uri(
     )
 
 
-def gen_single_prefill_cu(*args) -> None:
+def gen_single_prefill_cu(*args) -> Tuple[str, pathlib.Path]:
     gen_directory = FLASHINFER_GEN_SRC_DIR
     if not os.path.exists(gen_directory):
         os.makedirs(gen_directory)
-    file_name = f"{get_single_prefill_uri(*args)}.cu"
+    uri = get_single_prefill_uri(*args)
+    file_name = f"{uri}.cu"
+    path = gen_directory / file_name
     write_if_different(
-        gen_directory / file_name,
+        path,
         get_single_prefill_cu_str(*args),
     )
+    return uri, path
 
 
 def get_batch_prefill_cu_str(
@@ -252,12 +263,144 @@ def get_batch_prefill_uri(
     )
 
 
-def gen_batch_prefill_cu(*args) -> None:
+def gen_batch_prefill_cu(*args) -> Tuple[str, pathlib.Path]:
     gen_directory = FLASHINFER_GEN_SRC_DIR
     if not os.path.exists(gen_directory):
         os.makedirs(gen_directory)
-    file_name = f"{get_batch_prefill_uri(*args)}.cu"
+    uri = get_batch_prefill_uri(*args)
+    file_name = f"{uri}.cu"
+    path = gen_directory / file_name,
     write_if_different(
-        gen_directory / file_name,
         get_batch_prefill_cu_str(*args),
+    )
+    return uri, path
+
+
+def get_customize_single_decode_cu_str(
+    dtype_q: torch.dtype,
+    dtype_kv: torch.dtype,
+    dtype_o: torch.dtype,
+    head_dim: int,
+    additional_input_tensor_var_names: List[str],
+    additional_input_tensor_var_types: List[str],
+    additional_input_scalar_var_names: List[str],
+    additional_input_scalar_var_types: List[str],
+    variant_name: str,
+    variant_decl: str,
+) -> str:
+    template = jinja2.Template(customizable_single_decode_templ)
+    additional_params_decl = "".join(
+        [f"{dtype}* {var};\n"
+        for dtype, var in zip(
+            additional_input_tensor_var_types, additional_input_tensor_var_names
+        )] + [f"{dtype} {var};\n"
+        for dtype, var in zip(
+            additional_input_scalar_var_types, additional_input_scalar_var_names
+        )]
+    )
+    additional_params = "".join(
+        [f", {dtype}* {var}"
+        for dtype, var in zip(
+            additional_input_tensor_var_types, additional_input_tensor_var_names
+        )] + [f", {dtype} {var}"
+        for dtype, var in zip(
+            additional_input_scalar_var_types, additional_input_scalar_var_names
+        )]
+    )
+    additional_params_init = "".join(
+        [f", {var}({var})" for var in additional_input_tensor_var_names] +
+        [f", {var}({var})" for var in additional_input_scalar_var_names]
+    )
+    additional_func_params = "".join(
+        [f", torch::Tensor {var}" for var in additional_input_tensor_var_names] +
+        [f", {dtype} {var}" for dtype, var in zip(
+            additional_input_scalar_var_types, additional_input_scalar_var_names
+        )]
+    )
+    additional_params_data = "".join(
+        [f", static_cast<{dtype}*>({var}.data_ptr())"
+        for dtype, var in zip(
+            additional_input_tensor_var_types, additional_input_tensor_var_names
+        )] + [f", {var}"
+        for var in additional_input_scalar_var_names]
+    )
+
+    return template.render(
+        dtype_q=dtype_map[dtype_q],
+        dtype_kv=dtype_map[dtype_kv],
+        dtype_o=dtype_map[dtype_o],
+        head_dim=head_dim,
+        additional_params_decl=additional_params_decl,
+        additional_params=additional_params,
+        additional_params_init=additional_params_init,
+        variant_decl=variant_decl,
+        variant_name=variant_name,
+        additional_func_params=additional_func_params,
+        additional_params_data=additional_params_data
+    )
+
+
+def get_customize_single_prefill_cu_str(
+    dtype_q: torch.dtype,
+    dtype_kv: torch.dtype,
+    dtype_o: torch.dtype,
+    head_dim: int,
+    mask_mode: int,
+    additional_input_tensor_var_names: List[str],
+    additional_input_tensor_var_types: List[str],
+    additional_input_scalar_var_names: List[str],
+    additional_input_scalar_var_types: List[str],
+    variant_name: str,
+    variant_decl: str,
+) -> str:
+    template = jinja2.Template(customizable_single_prefill_templ)
+    additional_params_decl = "".join(
+        [f"{dtype}* {var};\n"
+        for dtype, var in zip(
+            additional_input_tensor_var_types, additional_input_tensor_var_names
+        )] + [f"{dtype} {var};\n"
+        for dtype, var in zip(
+            additional_input_scalar_var_types, additional_input_scalar_var_names
+        )]
+    )
+    additional_params = "".join(
+        [f", {dtype}* {var}"
+        for dtype, var in zip(
+            additional_input_tensor_var_types, additional_input_tensor_var_names
+        )] + [f", {dtype} {var}"
+        for dtype, var in zip(
+            additional_input_scalar_var_types, additional_input_scalar_var_names
+        )]
+    )
+    additional_params_init = "".join(
+        [f", {var}({var})" for var in additional_input_tensor_var_names] +
+        [f", {var}({var})" for var in additional_input_scalar_var_names]
+    )
+    additional_func_params = "".join(
+        [f", torch::Tensor {var}" for var in additional_input_tensor_var_names] +
+        [f", {dtype} {var}" for dtype, var in zip(
+            additional_input_scalar_var_types, additional_input_scalar_var_names
+        )]
+    )
+    additional_params_data = "".join(
+        [f", static_cast<{dtype}*>({var}.data_ptr())"
+        for dtype, var in zip(
+            additional_input_tensor_var_types, additional_input_tensor_var_names
+        )] + [f", {var}"
+        for var in additional_input_scalar_var_names]
+    )
+
+    return template.render(
+        dtype_q=dtype_map[dtype_q],
+        dtype_kv=dtype_map[dtype_kv],
+        dtype_o=dtype_map[dtype_o],
+        head_dim=head_dim,
+        mask_mode=mask_mode_literal[mask_mode],
+        additional_params_decl=additional_params_decl,
+        additional_params=additional_params,
+        additional_params_init=additional_params_init,
+        variant_decl=variant_decl,
+        variant_name=variant_name,
+        additional_func_params=additional_func_params,
+        additional_params_data=additional_params_data,
     )

python/flashinfer/prefill.py
@@ -15,7 +15,7 @@ limitations under the License.
 """
 
 import math
-from typing import Optional, Dict, Tuple, Union
+from typing import Optional, Dict, Tuple, Union, Any
 from types import SimpleNamespace
 import functools
 import torch
@@ -55,11 +55,9 @@ def compile_single_prefill_module(
     *args,
     verbose: bool = False,
 ):
-    gen_single_prefill_cu(*args)
-    uri = get_single_prefill_uri(*args)
+    uri, path = get_single_prefill_uri(*args)
     return load_cuda_ops(
-        uri,
-        [FLASHINFER_GEN_SRC_DIR / f"{uri}.cu"],
+        uri, [path],
         verbose=verbose,
     )
 
@@ -68,11 +66,9 @@ def compile_batch_prefill_module(
     *args,
     verbose: bool = False,
 ):
-    gen_batch_prefill_cu(*args)
-    uri = get_batch_prefill_uri(*args)
+    uri, path = get_batch_prefill_uri(*args)
     return load_cuda_ops(
-        uri,
-        [FLASHINFER_GEN_SRC_DIR / f"{uri}.cu"],
+        uri, [path],
         verbose=verbose,
     )
 
@@ -128,6 +124,21 @@ def get_batch_prefill_module(*args):
         else:
             _batch_prefill_modules[args] = compile_batch_prefill_module(*args)
     return _batch_prefill_modules[args]
+
+def single_prefill_with_kv_cache_with_jit_module(
+    jit_module: Any,
+    q: torch.Tensor,
+    k: torch.Tensor,
+    v: torch.Tensor,
+    *args,
+    kv_layout: str = "NHD",
+    window_left: int = -1,
+    return_lse: bool = False,
+) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+    tmp = _get_cache_buf("single_prefill_with_kv_cache_tmp", 32 * 1024 * 1024, q.device)
+    out = jit_module.run(
+        q, k, v, tmp, TensorLayout[kv_layout].value, window_left, return_lse, *args)
+    return out if return_lse else out[0]
 
 
 def single_prefill_with_kv_cache(

include/flashinfer/attention/decode.cuh
@@ -221,8 +221,6 @@ __global__ void SingleDecodeWithKVCacheKernel(const __grid_constant__
   const DTypeKV* v = params.v;
   DTypeO* o = params.o;
   float* lse = params.lse;
-  const float rope_rcp_scale = params.rope_rcp_scale;
-  const float rope_rcp_theta = params.rope_rcp_theta;
   uint32_t kv_chunk_size = params.kv_chunk_size;
 
   auto block = cg::this_thread_block();
@@ -247,6 +245,8 @@ __global__ void SingleDecodeWithKVCacheKernel(const __grid_constant__
   vec_t<float, vec_size> q_vec;
   vec_t<float, vec_size> freq;
   if constexpr (pos_encoding_mode == PosEncodingMode::kRoPELlama) {
+    const float rope_rcp_scale = params.rope_rcp_scale;
+    const float rope_rcp_theta = params.rope_rcp_theta;
 #pragma unroll
     for (uint32_t i = 0; i < vec_size; ++i) {
       freq[i] = rope_rcp_scale *
@@ -406,8 +406,6 @@ __global__ void BatchDecodeWithPagedKVCacheKernel(const __grid_constant__
   const bool* block_valid_mask = params.block_valid_mask;
   const uint32_t padded_batch_size = params.padded_batch_size;
   const uint32_t num_qo_heads = params.num_qo_heads;
-  const float rope_rcp_scale = params.rope_rcp_scale;
-  const float rope_rcp_theta = params.rope_rcp_theta;
   const bool partition_kv = params.partition_kv;
 
   constexpr uint32_t head_dim = bdx * vec_size;
@@ -442,6 +440,8 @@ __global__ void BatchDecodeWithPagedKVCacheKernel(const __grid_constant__
   vec_t<float, vec_size> freq;
   int32_t q_offset_val = q_offset == nullptr ? (kv_len - 1) : q_offset[batch_idx];
   if constexpr (POS_ENCODING_MODE == PosEncodingMode::kRoPELlama) {
+    const float rope_rcp_scale = params.rope_rcp_scale;
+    const float rope_rcp_theta = params.rope_rcp_theta;
 #pragma unroll
     for (uint32_t i = 0; i < vec_size; ++i) {
       freq[i] = rope_rcp_scale *

include/flashinfer/attention/decode_params.cuh
@@ -26,21 +26,16 @@
 namespace flashinfer {
 
 template <typename DTypeQ_, typename DTypeKV_, typename DTypeO_>
-struct DecodeParamsBase {
+struct SingleDecodeParams {
   using DTypeQ = DTypeQ_;
   using DTypeKV = DTypeKV_;
   using DTypeO = DTypeO_;
-  DTypeQ* q;
-  DTypeO* o;
-  float* lse;
-  float sm_scale;
-};
-
-template <typename DTypeQ, typename DTypeKV, typename DTypeO>
-struct SingleDecodeParams : public DecodeParamsBase<DTypeQ, DTypeKV, DTypeO> {
   using IdType = int32_t;
+  DTypeQ* q;
   DTypeKV* k;
   DTypeKV* v;
+  DTypeO* o;
+  float* lse;
   float* alibi_slopes;
   uint32_t kv_len;
   uint32_t num_qo_heads;
@@ -52,6 +47,7 @@ struct SingleDecodeParams : public DecodeParamsBase<DTypeQ, DTypeKV, DTypeO> {
   uint32_t head_dim;
   int32_t window_left;
   float logits_soft_cap;
+  float sm_scale;
   float rope_rcp_scale;
   float rope_rcp_theta;
   uint32_t kv_chunk_size;
@@ -62,9 +58,11 @@ struct SingleDecodeParams : public DecodeParamsBase<DTypeQ, DTypeKV, DTypeO> {
                                          QKVLayout kv_layout, uint32_t head_dim,
                                          int32_t window_left, float logits_soft_cap, float sm_scale,
                                          float rope_scale, float rope_theta)
-      : DecodeParamsBase<DTypeQ, DTypeKV, DTypeO>{q, o, /*lse=*/nullptr, sm_scale},
+      : q(q),
         k(k),
         v(v),
+        o(o),
+        lse(nullptr),
         alibi_slopes(alibi_slopes),
         kv_len(seq_len),
         num_qo_heads(num_qo_heads),
@@ -76,6 +74,7 @@ struct SingleDecodeParams : public DecodeParamsBase<DTypeQ, DTypeKV, DTypeO> {
         head_dim(head_dim),
         window_left(window_left),
         logits_soft_cap(logits_soft_cap),
+        sm_scale(sm_scale),
         rope_rcp_scale(1.f / rope_scale),
         rope_rcp_theta(1.f / rope_theta),
         kv_chunk_size(0) {}
@@ -105,17 +104,24 @@ struct SingleDecodeParams : public DecodeParamsBase<DTypeQ, DTypeKV, DTypeO> {
   }
 };
 
-template <typename DTypeQ, typename DTypeKV, typename DTypeO, typename IdType_>
-struct BatchDecodeParams : public DecodeParamsBase<DTypeQ, DTypeKV, DTypeO> {
+template <typename DTypeQ_, typename DTypeKV_, typename DTypeO_, typename IdType_>
+struct BatchDecodeParams {
+  using DTypeQ = DTypeQ_;
+  using DTypeKV = DTypeKV_;
+  using DTypeO = DTypeO_;
   using IdType = IdType_;
 
+  DTypeQ* q;
   IdType* q_offset;
   paged_kv_t<DTypeKV, IdType> paged_kv;
+  DTypeO* o;
+  float* lse;
   float* alibi_slopes;
   uint32_t padded_batch_size;
   uint32_t num_qo_heads;
   int32_t window_left;
   float logits_soft_cap;
+  float sm_scale;
   float rope_rcp_scale;
   float rope_rcp_theta;
 
@@ -131,14 +137,17 @@ struct BatchDecodeParams : public DecodeParamsBase<DTypeQ, DTypeKV, DTypeO> {
                                         float* alibi_slopes, uint32_t num_qo_heads,
                                         int32_t window_left, float logits_soft_cap, float sm_scale,
                                         float rope_scale, float rope_theta)
-      : DecodeParamsBase<DTypeQ, DTypeKV, DTypeO>{q, o, lse, sm_scale},
+      : q(q),
         q_offset(q_offset),
         paged_kv(paged_kv),
+        o(o),
+        lse(lse),
         alibi_slopes(alibi_slopes),
         padded_batch_size(0),
         num_qo_heads(num_qo_heads),
         window_left(window_left),
         logits_soft_cap(logits_soft_cap),
+        sm_scale(sm_scale),
         rope_rcp_scale(1.f / rope_scale),
         rope_rcp_theta(1.f / rope_theta),
         request_indices(nullptr),

include/flashinfer/attention/prefill.cuh
@@ -669,7 +669,9 @@ __device__ __forceinline__ void logits_mask(const typename AttentionVariant::Par
                    ? (kv_idx + qo_len > kv_len + q_idx || (kv_idx >= chunk_end))
                    : kv_idx >= chunk_end)) &&
             variant.LogitsMask(params, batch_idx, q_idx, kv_idx, qo_head_idx, kv_head_idx);
-        s_frag[fq][fkv][reg_id] = (mask) ? s_frag[fq][fkv][reg_id] : DTypeQKAccum(-math::inf);
+        s_frag[fq][fkv][reg_id] =
+            (mask) ? s_frag[fq][fkv][reg_id]
+                   : (variant.use_softmax ? DTypeQKAccum(-math::inf) : DTypeQKAccum(0.f));
       }
     }
   }
@@ -757,8 +759,10 @@ __device__ __forceinline__ void update_mdo_states(AttentionVariant variant,
 }
 
 template <uint32_t NUM_FRAGS_Q, uint32_t NUM_FRAGS_D, uint32_t NUM_FRAGS_KV,
-          SwizzleMode swizzle_mode, typename DTypeQ, typename DTypeKV, typename DTypeQKAccum>
-__device__ __forceinline__ void compute_sfm_v(smem_t<swizzle_mode>* v_smem,
+          SwizzleMode swizzle_mode, typename DTypeQ, typename DTypeKV, typename DTypeQKAccum,
+          typename AttentionVariant>
+__device__ __forceinline__ void compute_sfm_v(AttentionVariant variant,
+                                              smem_t<swizzle_mode>* v_smem,
                                               uint32_t* v_smem_offset_r,
                                               DTypeQKAccum (*s_frag)[NUM_FRAGS_KV][8],
                                               float (*o_frag)[NUM_FRAGS_D][8], float (*d)[2]) {
@@ -776,14 +780,16 @@ __device__ __forceinline__ void compute_sfm_v(smem_t<swizzle_mode>* v_smem,
     }
   }
 
+  if constexpr (variant.use_softmax) {
 #pragma unroll
-  for (uint32_t fq = 0; fq < NUM_FRAGS_Q; ++fq) {
+    for (uint32_t fq = 0; fq < NUM_FRAGS_Q; ++fq) {
 #pragma unroll
-    for (uint32_t fkv = 0; fkv < NUM_FRAGS_KV; ++fkv) {
-      if constexpr (std::is_same<DTypeQKAccum, float>::value) {
-        mma::rowsum_f16f16f32(d[fq], s_frag_f16[fq][fkv]);
-      } else {
-        mma::rowsum_f16f16f32(d[fq], s_frag[fq][fkv]);
+      for (uint32_t fkv = 0; fkv < NUM_FRAGS_KV; ++fkv) {
+        if constexpr (std::is_same<DTypeQKAccum, float>::value) {
+          mma::rowsum_f16f16f32(d[fq], s_frag_f16[fq][fkv]);
+        } else {
+          mma::rowsum_f16f16f32(d[fq], s_frag[fq][fkv]);
+        }
       }
     }
   }
@@ -1106,8 +1112,7 @@ __launch_bounds__(NUM_WARPS_Q* NUM_WARPS_KV* WARP_SIZE) void SinglePrefillWithKV
     const uint32_t kv_stride_n = params.kv_stride_n;
     const uint32_t kv_stride_h = params.kv_stride_h;
     const int32_t maybe_window_left = params.window_left;
-    const float log2_rope_rcp_scale = params.log2_rope_rcp_scale;
-    const float log2_rope_rcp_theta = params.log2_rope_rcp_theta;
+
     static_assert(sizeof(DTypeQ) == 2);
     static_assert(sizeof(DTypeO) == 2);
     const uint32_t lane_idx = threadIdx.x, warp_idx = get_warp_idx<NUM_WARPS_Q, NUM_WARPS_KV>();
@@ -1140,6 +1145,8 @@ __launch_bounds__(NUM_WARPS_Q* NUM_WARPS_KV* WARP_SIZE) void SinglePrefillWithKV
     float d[NUM_FRAGS_Q][2];
     float rope_freq[NUM_FRAGS_D / 2][4];
     if constexpr (POS_ENCODING_MODE == PosEncodingMode::kRoPELlama) {
+      const float log2_rope_rcp_scale = params.log2_rope_rcp_scale;
+      const float log2_rope_rcp_theta = params.log2_rope_rcp_theta;
       init_rope_freq<NUM_FRAGS_D>(rope_freq, log2_rope_rcp_scale, log2_rope_rcp_theta);
     }
     init_states<NUM_FRAGS_Q, NUM_FRAGS_D>(variant, o_frag, m, d);
@@ -1281,7 +1288,7 @@ __launch_bounds__(NUM_WARPS_Q* NUM_WARPS_KV* WARP_SIZE) void SinglePrefillWithKV
 
       // compute sfm*v
       compute_sfm_v<NUM_FRAGS_Q, NUM_FRAGS_D, NUM_FRAGS_KV, swizzle_mode_kv, DTypeQ, DTypeKV>(
-          &v_smem, &v_smem_offset_r, s_frag, o_frag, d);
+          variant, &v_smem, &v_smem_offset_r, s_frag, o_frag, d);
 
       block.sync();
       produce_kv<SharedMemFillMode::kFillZero, NUM_WARPS_Q, NUM_WARPS_KV, NUM_FRAGS_D,
@@ -1517,8 +1524,6 @@ __launch_bounds__(NUM_WARPS_Q* NUM_WARPS_KV* WARP_SIZE) void BatchPrefillWithRag
     const uint32_t kv_stride_n = params.kv_stride_n;
     const uint32_t kv_stride_h = params.kv_stride_h;
     const int32_t maybe_window_left = params.window_left;
-    const float log2_rope_rcp_scale = params.log2_rope_rcp_scale;
-    const float log2_rope_rcp_theta = params.log2_rope_rcp_theta;
 
     static_assert(sizeof(DTypeQ) == 2);
     static_assert(sizeof(DTypeO) == 2);
@@ -1561,6 +1566,8 @@ __launch_bounds__(NUM_WARPS_Q* NUM_WARPS_KV* WARP_SIZE) void BatchPrefillWithRag
     float rope_freq[NUM_FRAGS_D / 2][4];
 
     if constexpr (POS_ENCODING_MODE == PosEncodingMode::kRoPELlama) {
+      const float log2_rope_rcp_scale = params.log2_rope_rcp_scale;
+      const float log2_rope_rcp_theta = params.log2_rope_rcp_theta;
       init_rope_freq<NUM_FRAGS_D>(rope_freq, log2_rope_rcp_scale, log2_rope_rcp_theta);
     }
     init_states<NUM_FRAGS_Q, NUM_FRAGS_D>(variant, o_frag, m, d);
@@ -1717,7 +1724,7 @@ __launch_bounds__(NUM_WARPS_Q* NUM_WARPS_KV* WARP_SIZE) void BatchPrefillWithRag
 
       // compute sfm*v
       compute_sfm_v<NUM_FRAGS_Q, NUM_FRAGS_D, NUM_FRAGS_KV, swizzle_mode_kv, DTypeQ, DTypeKV>(
-          &v_smem, &v_smem_offset_r, s_frag, o_frag, d);
+          variant, &v_smem, &v_smem_offset_r, s_frag, o_frag, d);
 
       block.sync();
       produce_kv<SharedMemFillMode::kFillZero, NUM_WARPS_Q, NUM_WARPS_KV, NUM_FRAGS_D,
@@ -1805,8 +1812,6 @@ __launch_bounds__(NUM_WARPS_Q* NUM_WARPS_KV* WARP_SIZE) void BatchPrefillWithPag
     const paged_kv_t<DTypeKV, IdType>& paged_kv = params.paged_kv;
     const bool partition_kv = params.partition_kv;
     const int32_t maybe_window_left = params.window_left;
-    const float log2_rope_rcp_scale = params.log2_rope_rcp_scale;
-    const float log2_rope_rcp_theta = params.log2_rope_rcp_theta;
 
     static_assert(sizeof(DTypeQ) == 2);
     static_assert(sizeof(DTypeO) == 2);
@@ -1848,6 +1853,8 @@ __launch_bounds__(NUM_WARPS_Q* NUM_WARPS_KV* WARP_SIZE) void BatchPrefillWithPag
     float rope_freq[NUM_FRAGS_D / 2][4];
 
     if constexpr (POS_ENCODING_MODE == PosEncodingMode::kRoPELlama) {
+      const float log2_rope_rcp_scale = params.log2_rope_rcp_scale;
+      const float log2_rope_rcp_theta = params.log2_rope_rcp_theta;
       init_rope_freq<NUM_FRAGS_D>(rope_freq, log2_rope_rcp_scale, log2_rope_rcp_theta);
     }
     init_states<NUM_FRAGS_Q, NUM_FRAGS_D>(variant, o_frag, m, d);
@@ -2021,7 +2028,7 @@ __launch_bounds__(NUM_WARPS_Q* NUM_WARPS_KV* WARP_SIZE) void BatchPrefillWithPag
 
       // compute sfm*v
       compute_sfm_v<NUM_FRAGS_Q, NUM_FRAGS_D, NUM_FRAGS_KV, swizzle_mode_kv, DTypeQ, DTypeKV>(
-          &v_smem, &v_smem_offset_r, s_frag, o_frag, d);
+          variant, &v_smem, &v_smem_offset_r, s_frag, o_frag, d);
 
       block.sync();
       page_produce_kv<true, NUM_WARPS_Q, NUM_WARPS_KV, NUM_FRAGS_D, NUM_FRAGS_KV>(

commit 3fa5087a1a0430e22fc2d2a9b33ff0c1c7731e92

bugfix: fix JIT compilation of prefill kernels (#536)
Some bugs were introduced in #534, this PR fix these issues.

python/flashinfer/jit/attention.py
@@ -269,8 +269,9 @@ def gen_batch_prefill_cu(*args) -> Tuple[str, pathlib.Path]:
         os.makedirs(gen_directory)
     uri = get_batch_prefill_uri(*args)
     file_name = f"{uri}.cu"
-    path = gen_directory / file_name,
+    path = gen_directory / file_name
     write_if_different(
+        path,
         get_batch_prefill_cu_str(*args),
     )
     return uri, path

python/flashinfer/prefill.py
@@ -55,7 +55,7 @@ def compile_single_prefill_module(
     *args,
     verbose: bool = False,
 ):
-    uri, path = get_single_prefill_uri(*args)
+    uri, path = gen_single_prefill_cu(*args)
     return load_cuda_ops(
         uri, [path],
         verbose=verbose,
@@ -66,7 +66,7 @@ def compile_batch_prefill_module(
     *args,
     verbose: bool = False,
 ):
-    uri, path = get_batch_prefill_uri(*args)
+    uri, path = gen_batch_prefill_cu(*args)
     return load_cuda_ops(
         uri, [path],
         verbose=verbose,