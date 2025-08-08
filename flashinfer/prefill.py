YASoftwareDev/flashinfer

commit e608c38f5744fd4929608f5ccfe77f88cde471ec

Add mypy to pre-commit (#1179)

flashinfer/prefill.py
@@ -80,7 +80,7 @@ def get_fmha_module(
             dtype_idx,
             head_dim_qk,
             head_dim_vo,
-            pos_encoding_mode,
+            pos_encoding_mode.value,
             use_sliding_window,
             use_logits_soft_cap,
         ).build_and_load()

flashinfer/cudnn/prefill.py
@@ -143,11 +143,7 @@ if CUDNN_AVAILABLE:
             if q.dim() == 3:
                 h_qo, d_qk = q.shape[1], q.shape[2]
             elif q.dim() == 4:
-                h_qo, d_qk = (
-                    q.shape[1],
-                    q.shape[2],
-                    q.shape[3],
-                )
+                h_qo, d_qk = q.shape[2], q.shape[3]
             else:
                 raise ValueError(f"Invalid query tensor shape: {q.shape}")
 

.pre-commit-config.yaml
@@ -54,3 +54,8 @@ repos:
         types_or: [c++, c, cuda]
         exclude: |
           (?x)^(3rdparty/.* flashinfer/jit/aot_config.py)$
+
+  -   repo: https://github.com/pre-commit/mirrors-mypy
+      rev: ''  # Use the sha / tag you want to point at
+      hooks:
+      -   id: mypy

benchmarks/bench_mixed_attention.py
@@ -185,8 +185,8 @@ if __name__ == "__main__":
         full_kv_len = np.random.randint(2000, 16000, size=bsz)
         p_q_lens = []
         p_kv_lens = []
-        d_q_len = []
-        d_kv_len = []
+        d_q_lens = []
+        d_kv_lens = []
 
         for i in range(bsz):
             if i % stride == 0:

ci/scripts/jenkins/git_utils.py
@@ -30,13 +30,13 @@ DRY_RUN = object()
 
 def compress_query(query: str) -> str:
     query = query.replace("\n", "")
-    query = re.sub("\s+", " ", query)
+    query = re.sub(r"\s+", " ", query)
     return query
 
 
 def post(url: str, body: Optional[Any] = None, auth: Optional[Tuple[str, str]] = None):
     logging.info(f"Requesting POST to {url} with {body}")
-    headers = {}
+    headers: Dict[Any, Any] = {}
     req = request.Request(url, headers=headers, method="POST")
     if auth is not None:
         auth_str = base64.b64encode(f"{auth[0]}:{auth[1]}".encode())
@@ -46,9 +46,8 @@ def post(url: str, body: Optional[Any] = None, auth: Optional[Tuple[str, str]] =
         body = ""
 
     req.add_header("Content-Type", "application/json; charset=utf-8")
-    data = json.dumps(body)
-    data = data.encode("utf-8")
-    req.add_header("Content-Length", len(data))
+    data = json.dumps(body).encode("utf-8")
+    req.add_header("Content-Length", str(len(data)))
 
     with request.urlopen(req, data) as response:
         return response.read()
@@ -119,9 +118,8 @@ class GitHubRepo:
         logging.info(f"Requesting {method} to {full_url} with {body}")
         req = request.Request(full_url, headers=self.headers(), method=method.upper())
         req.add_header("Content-Type", "application/json; charset=utf-8")
-        data = json.dumps(body)
-        data = data.encode("utf-8")
-        req.add_header("Content-Length", len(data))
+        data = json.dumps(body).encode("utf-8")
+        req.add_header("Content-Length", str(len(data)))
 
         try:
             with request.urlopen(req, data) as response:
@@ -206,12 +204,11 @@ def find_ccs(body: str) -> List[str]:
     matches = re.findall(r"(cc( @[-A-Za-z0-9]+)+)", body, flags=re.MULTILINE)
     matches = [full for full, last in matches]
 
-    reviewers = []
+    reviewers = set()
     for match in matches:
         if match.startswith("cc "):
             match = match.replace("cc ", "")
         users = [x.strip() for x in match.split("@")]
-        reviewers += users
+        reviewers.update(users)
 
-    reviewers = set(x for x in reviewers if x != "")
-    return list(reviewers)
+    return [x for x in reviewers if x != ""]

docs/conf.py
@@ -2,6 +2,7 @@ import os
 import sys
 import warnings
 from pathlib import Path
+from typing import Any, List
 
 # import tlcpack_sphinx_addon
 # Configuration file for the Sphinx documentation builder.
@@ -64,16 +65,12 @@ todo_include_todos = False
 
 html_theme = "furo"  # "sphinx_rtd_theme"
 
-templates_path = []
+templates_path: List[Any] = []
 
-html_static_path = []
+html_static_path = ["_static"]
 
 html_theme_options = {
     "logo_only": True,
-}
-
-html_static_path = ["_static"]
-html_theme_options = {
     "light_logo": "FlashInfer-white-background.png",
     "dark_logo": "FlashInfer-black-background.png",
 }

flashinfer/artifacts.py
@@ -19,7 +19,7 @@ import re
 import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-import requests
+import requests  # type: ignore[import-untyped]
 
 from .jit.core import logger
 from .jit.cubin_loader import FLASHINFER_CUBINS_REPOSITORY, get_cubin

flashinfer/autotuner.py
@@ -583,7 +583,7 @@ class AutoTuner:
 
         generated_profiles: List[OptimizationProfile] = []
 
-        dynamic_dims = []
+        dynamic_dims: List[Tuple[Any, ...]] = []
 
         for spec in tuning_config.dynamic_tensor_specs:
             assert inspect.isfunction(spec.gen_tuning_buckets) or isinstance(
@@ -591,12 +591,14 @@ class AutoTuner:
             ), "The given dynamic dimension must provide a opt value generation function or a list of opt values"
             if inspect.isfunction(spec.gen_tuning_buckets):
                 opt_shapes = spec.gen_tuning_buckets(
-                    base_profile.shapes[spec.input_idx][spec.dim_idx].val
+                    base_profile.shapes[spec.input_idx][spec.dim_idx]._opt()
                 )
             else:
                 opt_shapes = spec.gen_tuning_buckets
-            opt_shapes_max = tuple(opt_shapes[1:]) + (float("inf"),)
-            opt_shapes_max = {v1: v2 for v1, v2 in zip(opt_shapes, opt_shapes_max)}
+            opt_shapes_max = {
+                v1: v2
+                for v1, v2 in zip(opt_shapes, tuple(opt_shapes[1:]) + (float("inf"),))
+            }
             dynamic_dims.append(
                 (spec.input_idx, spec.dim_idx, opt_shapes_max, opt_shapes)
             )
@@ -617,10 +619,12 @@ class AutoTuner:
                 )
 
             # Adjust the profile to satisfy the constraints
-            for spec in tuning_config.constraint_specs:
-                min_value = opt_value = max_value = spec.infer_shape(p.get_opt_shapes())
-                p.shapes[spec.input_idx][spec.dim_idx] = DynamicDim(
-                    min_value, opt_value, max_value
+            for constraint_spec in tuning_config.constraint_specs:
+                min_value = opt_value = max_value = constraint_spec.infer_shape(
+                    p.get_opt_shapes()
+                )
+                p.shapes[constraint_spec.input_idx][constraint_spec.dim_idx] = (
+                    DynamicDim(min_value, opt_value, max_value)
                 )
             generated_profiles.append(p)
             logger.debug(f"[Autotuner]: generated profile: {p}")
@@ -651,8 +655,8 @@ class AutoTuner:
             )
 
         # associated dimensions dependent on other free dynamic dimensions, so assign -1 in the profile
-        for spec in tuning_config.constraint_specs:
-            base_profile[spec.input_idx][spec.dim_idx] = -1
+        for constraint_spec in tuning_config.constraint_specs:
+            base_profile[constraint_spec.input_idx][constraint_spec.dim_idx] = -1
 
         return tuple(tuple(shape) for shape in base_profile)
 

commit eb863952c7beaff818478cd1e12fd1065dadf378

bugfix: Fix crash when pos_encoding_mode is passed as int (#1413)
This PR is to fix this issue caused by
https://github.com/flashinfer-ai/flashinfer/commit/e608c38f5744fd4929608f5ccfe77f88cde471ec
```  
File "/scratch/repo/flashinfer/flashinfer/prefill.py", line 83, in get_fmha_module                                                                                                               pos_encoding_mode.value,                                                                                                                                                                       ^^^^^^^^^^^^^^^^^^^^^^^                                                                                                                                                                    AttributeError: 'int' object has no attribute 'value' 
```

Update get_fmha_module to expect pos_encoding_mode as an Enum instead of
an int; raises error otherwise.

cc @yzh119

flashinfer/prefill.py
@@ -66,7 +66,7 @@ def get_fmha_module(
     dtype_idx: torch.dtype,
     head_dim_qk: int,
     head_dim_vo: int,
-    pos_encoding_mode: PosEncodingMode,
+    pos_encoding_mode: int,
     use_sliding_window: bool,
     use_logits_soft_cap: bool,
     use_fp16_qk_reduction: bool = False,
@@ -79,7 +79,7 @@ def get_fmha_module(
             dtype_idx,
             head_dim_qk,
             head_dim_vo,
-            pos_encoding_mode.value,
+            pos_encoding_mode,
             use_sliding_window,
             use_logits_soft_cap,
         ).build_and_load()